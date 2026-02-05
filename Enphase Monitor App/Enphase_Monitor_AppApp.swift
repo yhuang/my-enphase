0403129ff190391739b08adbe752ccae987c3010
Jimmy Huang <jimmy.huang@blackline.com>
2026-02-05 14:13:18 -0800

    Initial Commit

---
+++ Enphase Monitor App/Enphase_Monitor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enphase_Monitor_AppApp.swift
+//  Enphase Monitor App
+//
+//  Created by Jimmy Huang on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Enphase_Monitor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
